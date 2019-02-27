b992dae0ec17b6b42efb5e8feba1b04281a38a0b
QingHang <liqinghang@daojia.com>
2019-02-27 14:16:29 +0800

    test

--- src/main/java/com/qh/cloud/test/guava/GuavaTest.java
+++ src/main/java/com/qh/cloud/test/guava/GuavaTest.java
@@ -5,11 +5,16 @@ import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import com.google.common.base.Joiner;
 import com.google.common.base.Predicate;
 import com.google.common.collect.Collections2;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 
 /**
  * @Description Guava 的测试
@@ -55,6 +60,28 @@ public class GuavaTest {
 		System.out.println(ints);
 		ints.addAll(Arrays.asList(8, 9));
 		System.out.println(ints);
+		collectionCreate();
+	}
+
+	/**
+	 * 集合的创建
+	 */
+	public static void collectionCreate(){
+		//普通
+		List<String> list = Lists.newArrayList();
+		list = Lists.newArrayList("L","Q","H");
+		System.out.println(list.toString());
+
+		Set<String> set = Sets.newHashSet();
+		set = Sets.newHashSet("L","Q","H");
+		System.out.println(set.toString());
+
+		Map<String,String> map = Maps.newHashMap();
+		map.put("lqh","LQH");
+
+		//不可变
+		ImmutableList.of("L","Q","H");
 
 	}
+
 }
